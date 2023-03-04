a343f3fe05a1e54d75fba9afd4398be49854945a
Tom Haynes <tomhaynes94@gmail.com>
2023-03-04 16:17:34 +0000

    Dynamically setting styles on the className prop by adding a class depending on whether the state is true/false

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -26,19 +26,12 @@ const CourseInput = props => {
   };
 
   return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label 
-          style={{ color: !isValid ? 'red' : 'black' }}
-        >
+    <form onSubmit={ formSubmitHandler }>
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>
           Course Goal
         </label>
         <input
-          style=
-            {{ 
-              borderColor: !isValid ? 'red' : '#ccc', 
-              background: !isValid ? 'salmon' : 'transparent' 
-            }} 
           type="text" 
           onChange={ goalInputChangeHandler } 
         />
